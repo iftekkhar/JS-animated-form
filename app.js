988b77e80597dbd2c7c3dbf96b0229416cfa2675
Iftekhar Anam <iftekhar_anam@hotmail.com>
2020-06-02 00:10:04 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function animatedForms() {
+    const arrows = document.querySelectorAll(".fa-arrow-down");
+    
+    arrows.forEach(arrow =>{
+        arrow.addEventListener('click', () =>{
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+
+            //check for Validation
+            if(input.type === "text" && validUser(input)){
+                nextSlide(parent, nextForm);
+            } else if(input.type === 'email' && validateEmail (input)){
+                nextSlide(parent, nextForm);
+            }else if(input.type === 'password' && validUser (input)){
+                nextSlide(parent, nextForm);
+            } else{
+                parent.style.animation ="shake 0.5s ease";
+            }
+            //Rid of animation
+            parent.addEventListener("animationend", () =>{
+                parent.style.animation="";
+            });
+        });
+    });
+}
+
+function validUser(user){
+    if(user.value.length <6 ){
+        error('rgb(189,87,87');
+    } else {
+        error('rgb(87,189,130');
+        return true;
+    }
+}
+function validateEmail(email){
+    const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(validation.test(email.value)){
+        error('rgb(87,189,130');
+        return true;
+    }else{
+        error('rgb(189,87,87');
+    }
+}
+
+function error(color) {
+    document.body.style.backgroundColor = color;    
+}
+function nextSlide(parent, nextForm) {
+    parent.classList.add('inactive');
+    parent.classList.remove('active');
+    nextForm.classList.add('active');
+}
+animatedForms();
